fc55566e5e1aa5c02c2de9e8dcb96ad97d5f3825
tranphucanh <anhtp@algolabs.vn>
2025-03-07 01:06:24 +0700

    chore rebuild layout #2

--- Do_an/js/main.js
+++ Do_an/js/main.js
@@ -1,18 +1,18 @@
-function search(){
-    var k =document.getElementById('kw');
-    if(k != null){
-        k= k.value
+function search() {
+    var k = document.getElementById('kw');
+    if (k != null) {
+        k = k.value
 
-        var items=document.querySelectorAll("div.img > a")
-        for (var i=0;i<=items.length;i++){
-            var name=items[i].getElementsByClassName("caption")[0].innerText;
-            
-            if(name.toLowerCase().indexOf(k)>=0){
-                items[i].parentElement.style.height="360px";
-                items[i].parentElement.style.display="inline-block";
+        var items = document.querySelectorAll("div.img > a")
+        for (var i = 0; i <= items.length; i++) {
+            var name = items[i].getElementsByClassName("caption")[0].innerText;
+
+            if (name.toLowerCase().indexOf(k) >= 0) {
+                items[i].parentElement.style.height = "480px";
+                items[i].parentElement.style.display = "inline-block";
             }
             else
-                items[i].parentElement.style.display="none";
+                items[i].parentElement.style.display = "none";
         }
     }
 }
